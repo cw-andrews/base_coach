39f535d01d347d7824fe38a32f3d537259b41a2d
cw-andrews <cwandrews@protonmail.com>
2020-06-27 15:51:07 -0400

    Updated variable names to use src and dst rather than infile/outfile [master]

--- src/base_coach/files.py
+++ src/base_coach/files.py
@@ -3,24 +3,24 @@ from typing import Iterable
 
 
 def should_continue_processing(
-    infile: Path, outfile: Path, force: bool = False
+    srcfile: Path, dstfile: Path, force: bool = False
 ) -> bool:
     """
     Determines whether or not processing should continue
     """
 
-    if infile.exists() and infile.is_file():
-        return any(check_meets_condition(infile, outfile, force))
+    if srcfile.exists() and srcfile.is_file():
+        return any(check_meets_condition(srcfile, dstfile, force))
     else:
-        raise FileNotFoundError(f"'{infile}' wasn't found or isn't a file...")
+        raise FileNotFoundError(f"'{srcfile}' wasn't found or isn't a file...")
 
 
-def check_meets_condition(infile: Path, outfile: Path, force: bool) -> Iterable[bool]:
+def check_meets_condition(srcfile: Path, dstfile: Path, force: bool) -> Iterable[bool]:
     """
     Takes an infile path, an outfile path, and whether or not the job was forced, and returns
     whether or not the existing conditions meet the requirements for processing to continue.
     """
 
     yield force
-    yield not outfile.exists()
-    yield infile.lstat().st_mtime_ns > outfile.lstat().st_mtime_ns
+    yield not dstfile.exists()
+    yield srcfile.lstat().st_mtime_ns > dstfile.lstat().st_mtime_ns
